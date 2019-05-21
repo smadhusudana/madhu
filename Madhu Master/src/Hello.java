051cbc38f1aa347a42b30d7f33d3c9ad20296aee
suryadevaramadhu <suryadevaramadhu@DESKTOP-5FR7BNN>
2019-05-21 14:18:31 -0700

    First Commit
    Uploading all the files

---
+++ Madhu Master/src/Hello.java
@@ -0,0 +1,32 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		String s = "abc";
+		
+		switch(s) {
+		
+		case "abc": 
+			
+			System.out.println("One");
+		
+			break;
+			
+		case "pqr":
+			System.out.println("Two");
+			break;
+			
+		default:
+			
+			System.out.println("None");
+			
+		
+		
+		}
+	
+	
+
+	}
+
+}
---
+++ Madhu Master/src/IterationDemo.java
@@ -0,0 +1,17 @@
+
+public class IterationDemo {
+
+	public static void main(String[] args) {
+		
+		int i =0;
+		
+		
+	do
+		{
+			System.out.println("Hello Madhu");
+			i++;
+		} while(i<=20);
+
+	}
+
+}
